f3bdb1f2acdbc71a2af835835c5be5908e9f2d4b
Gustavo Mazzotti <gustavo@yupistudios.com>
2017-01-05 17:30:52 -0300

    Cena básica de Ingame e Script de Procura por Partida.

---
+++ Assets/SearchingMatchScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SearchingMatchScript : MonoBehaviour {
+
+
+	public Image AdvImage;
+
+	public Sprite Image;
+
+	void OnEnable() {
+		StartCoroutine ("Finding");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Finding() {
+		yield return new WaitForSeconds(2);
+		print(Time.time);
+		AdvImage.sprite = Image;
+		yield return new WaitForSeconds(1);
+		CallToBattle ();
+	}
+
+	public void CallToBattle(){
+		Application.LoadLevel ("Game");
+	}
+
+}
