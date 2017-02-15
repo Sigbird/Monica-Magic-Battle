16996c1a93ce6ca41ab2ec3fd0383269739ae09e
Ivan Quirino <ivan@yupistudios.com>
2017-02-15 17:38:36 -0300

    resolve #5 NetworkMessage com testes

---
+++ Assets/MMagicRush/Editor/Tests/NetworkMessageTest.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using YupiPlay;
+using System.Text;
+
+[TestFixture]
+public class NetworkMessageTest : AssertionHelper {
+    char messageClass = 'P';
+    char messageType = 'S';
+    string testMessage = "mensagem de teste";    
+
+    NetworkMessage message;
+    NetworkMessage receivedMessage;
+
+    [SetUp]
+    public void Init()
+    {
+        var testBytes = Encoding.UTF8.GetBytes(testMessage);        
+        message = new NetworkMessage(messageClass, messageType, testBytes);
+       
+        var bytes = message.ToBytes();
+        receivedMessage = new NetworkMessage(bytes);
+    }
+
+    [Test]
+	public void MessageClassTest() {                              
+        Debug.Log(receivedMessage.MessageClass);
+        Expect(receivedMessage.MessageClass, EqualTo(messageClass));       
+	}
+
+    [Test]
+    public void MessageTypeTest()
+    {
+        Debug.Log(receivedMessage.MessageType);
+        Expect(receivedMessage.MessageType, EqualTo(messageType));        
+    }
+
+    [Test]
+    public void MessageIdTest()
+    {
+        Debug.Log(message.MessageId.ToString());
+        Debug.Log(receivedMessage.MessageId.ToString());
+        Expect(receivedMessage.MessageId.ToString(), EqualTo(message.MessageId.ToString()));        
+    }
+
+    [Test]
+    public void DataTest()
+    {        
+        string receivedTestMessage = Encoding.UTF8.GetString(receivedMessage.Data);
+        Debug.Log(receivedTestMessage);
+        Expect(receivedTestMessage, EqualTo(testMessage));        
+    }
+
+    [Test]
+    public void DataLenghtTest()
+    {
+        int sentLength = Encoding.UTF8.GetBytes(testMessage).Length;
+        int receivedLength = receivedMessage.Data.Length;
+
+        Expect(receivedLength, EqualTo(sentLength));
+    }
+}
--- Assets/MMagicRush/Scripts/Multiplayer/NetworkMessage.cs
+++ Assets/MMagicRush/Scripts/Multiplayer/NetworkMessage.cs
@@ -5,42 +5,52 @@ namespace YupiPlay
 	public class NetworkMessage
 	{		
 		//unique Id
-		public Guid MessageId {get;}
+		public Guid MessageId { get; private set; }
 
 		//to group messages to deliver to classes of objects, global routing
 		public char MessageClass;
 
 		//the type of message
-		public char Type;
+		public char MessageType;
 
 		//message payload
-		byte[] Data;
+		public byte[] Data;
 
-		public NetworkMessage (char messageClass = '0', char type = '0', byte[] data = null)
+		public NetworkMessage (char messageClass = '0', char messageType = '0', byte[] data = null)
 		{
 			MessageId = Guid.NewGuid();
 			MessageClass = messageClass;
-			Type = type;
+			MessageType = messageType;
 			Data = data;
 		}
+
+        public NetworkMessage(byte[] data)
+        {
+            MessageClass = (char) data[0];
+            MessageType = (char) data[1];
+
+            byte[] guidBytes = new byte[16];            
+            Array.Copy(data, 2, guidBytes, 0, 16);
+            MessageId = new Guid(guidBytes);
+
+            Data = new byte[data.Length - 18];
+            Array.Copy(data, 18, Data, 0, data.Length - 18);
+        }
 
 		public byte[] ToBytes() {
 			byte[] messageId = MessageId.ToByteArray();
 			byte messageClass = (byte) MessageClass;
-			byte type = (byte) Type;
+			byte type = (byte) MessageType;
 
 			byte[] bytes = new byte[2 + messageId.Length + Data.Length];
 			bytes[0] = messageClass;
 			bytes[1] = type;
 
-			Array.Copy(messageId, 0, bytes, 2 + (messageId.Length - 1), messageId.Length);
-
+			Array.Copy(messageId, 0, bytes, 2, messageId.Length);
+            Array.Copy(Data, 0, bytes, 2 + messageId.Length, Data.Length);
 
 			return bytes;
 		}
-
-
-
 	}
 }
 
