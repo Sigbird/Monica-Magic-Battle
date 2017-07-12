626d738de9fc8fba22b27e8213992a3b4c79696e
Ivan Quirino <ivan@yupistudios.com>
2017-07-12 19:10:50 -0300

    Início do NetworkClock e NetIOQueue.cs e implementação do Lockstep

---
+++ Assets/MMagicRush/Scripts/Multiplayer/Lockstep/NetClock.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+using YupiPlay.MMB.Lockstep;
+using System.Collections.Generic;
+
+namespace YupiPlay.MMB.LockStep {
+
+    public class NetClock : MonoBehaviour {
+        public const float TurnTime = 0.2f;
+
+        public static NetClock Instance = null;
+
+        private long Turn = 1;
+        private Coroutine ClockCoroutine = null;
+        private bool IsClockRunning = false;
+        private NetIOQueue Queue = null;
+
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            } else {
+                Destroy(this.gameObject);
+            }
+        }       
+
+        private IEnumerator RunClock() {
+            IsClockRunning = true;
+
+            while (IsClockRunning) {                               
+                yield return new WaitForSecondsRealtime(TurnTime);
+
+                AddTurnToQueue(Turn);
+                SendTurn(Turn);
+
+                if (Turn - 2 > 2) {
+                    PlayTurn(Turn - 2);
+                }
+
+                Turn++;
+            }
+        }
+
+        public void StartClock() {
+            Queue = NetIOQueue.Instance;
+            ClockCoroutine = StartCoroutine(RunClock());
+        }
+
+        public void StopClock() {
+            IsClockRunning = false;
+            StopCoroutine(ClockCoroutine);
+        }
+
+        public long GetTurn() {
+            return Turn;
+        }
+
+        private void AddTurnToQueue(long turn) {
+            NetCommand cmd = new NetCommand(turn, NetCommand.TURN);
+            Queue.AddToOut(cmd);
+        }
+
+        private void PlayTurn(long turn) {
+            Debug.Log("Playing Turn:" + turn);
+            //notify objects to Play Turn;
+        }
+
+        private void SendTurn(long turn) {
+            List<NetCommand> cmds = Queue.GetOutCommandsForTurn(turn);
+            Debug.Log("Sending Turn: " + turn);
+        }
+
+        // Use this for initialization
+        void Start() {
+            StartClock();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
+
---
+++ Assets/MMagicRush/Scripts/Multiplayer/Lockstep/NetCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace YupiPlay.MMB.Lockstep {
+    class NetCommand {
+        public const string TURN = "TURN";        
+
+        //HELLO mensagem inical
+        public const string HELLO = "HELLO";
+
+        public const string START = "START";
+        public const string END = "END";
+
+        //MOVE move herói 
+        public const string MOVE = "MOVE";
+        
+        //ATK ataca uma alvo
+        public const string ATK = "ATK";
+
+        //SPAWN cria uma unidade
+        public const string SPAWN = "SPAWN";
+        
+        //ACTION ação genéria, cartas globais
+        public const string ACTION = "ACTION";
+
+        //SPELL para cartas com alvos
+        public const string SPELL = "SPELL";
+
+        protected string Command = TURN;
+        protected long Turn      = 1;
+
+        public NetCommand(long turn, string command) {
+            Command = command;
+            Turn    = turn;
+        }
+
+        public string GetCommand() {
+            return Command;
+        }
+        public long GetTurn() {
+            return Turn;
+        }
+
+        override public string ToString() {
+            return "Turn: " + Turn + ", " + "Cmd: " + Command;
+        }
+        
+    }
+}
