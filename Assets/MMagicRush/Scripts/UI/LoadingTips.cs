47d45a43330f03abe5c7021b96716c70d533c401
Gustavo Mazzotti <gustavo@yupistudios.com>
2019-03-29 09:00:17 -0300

    Habilidade Especial do Cascão e Loading Tips

---
+++ Assets/MMagicRush/Scripts/UI/LoadingTips.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class LoadingTips : MonoBehaviour {
+	public string[] texts;
+	public Text textbox;
+	// Use this for initialization
+	void Start () {
+		textbox.text = texts [Random.Range (0, texts.Length - 1)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
